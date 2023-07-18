2fa70b8ddd872a6227176bc34d1f22555bbf0f66
lucasgriesang <127210973+lucasgriesang@users.noreply.github.com>
2023-07-18 09:42:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function ad() {
+    var nome = document.getElementById("admin").value
+    if (nome === "admin") {
+        window.alert("o nome esta coreto ");
+    }
+    else {
+        alert("nome incoreto ");
+    }
+    var senha = document.getElementById("123").value
+    if (senha === "123") {
+        window.alert("o senha esta coreto ");
+    }
+    else {
+        alert("senha incoreto ");
+    }
+}
